e722759cc67fe13a6d11204508927d361c5c51ea
root <misiewiczp@gmail.com>
2018-12-10 22:47:59 +0000

    SocketIO sensor monitoring

--- webctrl.app/server.py
+++ webctrl.app/server.py
@@ -1,10 +1,32 @@
+import threading
+import time
 from flask import Flask,render_template
 from flask_socketio import SocketIO
+import eventlet
+import lcm
+from arc import control_t, distance_t, imu_t
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'kaksadhajkdwyeuiqqxmZB'
 #app.config['host']='0.0.0.0'
 socketio = SocketIO(app)
+lc = lcm.LCM()
+runLoop = True
+
+lastMsgTime = {"DSTN":0}
+TIME_LIMIT=0.5 # update twice a second
+
+@app.before_first_request
+def activate_job():
+    def run_job():
+        while runLoop:
+            lc.handle()
+            eventlet.sleep(0) # otherwise emit's wait for long time
+    thread = threading.Thread(target=run_job)
+    thread.start()
+
 
 @app.route('/')
 def sessions():
@@ -18,5 +40,27 @@ def handle_CTRL_event(json, methods=['GET', 'POST']):
     print('CTRL req: ' + str(json))
     socketio.emit('CTRL_LOG', json, callback=messageReceived)
 
+def skip_emit(queue, json):
+    global lastMsgTime
+    if time.time() - lastMsgTime[queue] > TIME_LIMIT:
+        socketio.emit(queue, json)
+        lastMsgTime[queue]=time.time()
+
+def lcm_dstn(channel, data):
+    m = distance_t.decode(data)
+    skip_emit('DSTN', {'m':m.measure})
+
+def lcm_ctrl_log(channel, data):
+    m = control_t.decode(data)
+    socketio.emit('CTRL_LOG', {'m':m.motor, 's':m.servo})
+
+lc.subscribe("DSTN", lcm_dstn)
+lc.subscribe("CTRL_LOG", lcm_ctrl_log)
+
+
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', debug=False)
+    try:
+        socketio.run(app, host='0.0.0.0', debug=False)
+        runLoop = False
+    except KeyboardInterrupt:
+        runLoop = False
